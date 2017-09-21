3bb44511932a472cd8f39db4193378928b96414d
Joshua James <burke.joshua.james@gmail.com>
2017-09-21 16:57:53 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,11 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+# First horizontal combinations
+# Then vertical combinations
+# Then diagonal combinations
+WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+]
